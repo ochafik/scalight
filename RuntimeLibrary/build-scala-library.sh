0324f14df448909e00220aee074db99275d5232d
Olivier Chafik <olivier.chafik@gmail.com>
2012-05-12 00:18:15 +0100

    script update

--- RuntimeLibrary/build-scala-library.sh
+++ RuntimeLibrary/build-scala-library.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 if [[ -d scala-scalight ]] ; then
-	cd scala-scalight
+	cd scala-scalight || exit 1
 	
 	echo "Resetting sources"
-	git checkout src
+	git checkout src || exit 1
 	
 	echo "Deleting previous builds"
-	ant all.clean
+	pwd
+	ant all.clean || exit 1
 	
-	cd ..
 else
 	echo "Checking out scala"
 	git checkout https://github.com/scala/scala.git scala-scalight
 	
-	cd scala-scalight
+	cd scala-scalight || exit 1
 	
-	dos2unix *.sh tools/*.sh
+	dos2unix *.sh tools/*.sh || exit 1
 	
-	if [[ -d /System ]] then 
+	if [[ -d /System ]] ; then 
 		echo "Fixing mac build"
 		#local version=$(echo $sha1 | sed 's/ \?.*//')
 		#local version=${sha1% ?$jar_name}
@@ -26,28 +26,28 @@ else
 		sed -i.bak -E 's/\$\{sha1% \?\$jar_name\}/\$\(echo \$sha1 \| sed '"'s\/ \\\?.*\/\/'\)/" tools/binary-repo-lib.sh
 	fi
 	
-	./pull-binary-libs.sh
+	./pull-binary-libs.sh || exit 1
 fi
 
 SCALA_SCALIGHT_HOME=`pwd`
 
 echo "Build scala"
-ant	
+ant || exit 1
 
 echo "Applying first patch"
-git apply ../scalight-compiler.diff
+git apply ../scalight-compiler.diff || exit 1
 
 echo "Deleting the 'locker' compiler"
 rm -fR build/locker/all.complete build/locker/compiler.complete build/locker/classes/compiler
 
 echo "Recompiling the 'locker' compiler"
-ant
+ant || exit 1
 	
 echo "Applying second patch"
-git apply ../scalight-library.diff
+git apply ../scalight-library.diff || exit 1
 
 echo "Removing @specialized annotations"
 find src/library -name '*.scala' -exec sed -i.bak -E 's/@specialized\(.*\)//' '{}' ';'
 	
 echo "Recompiling 'quick'"	
-ant quick.clean build
+ant quick.clean build || exit 1
